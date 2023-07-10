39c40b9af04175cb9518aacadbf004efe2dcc39a
Alex Melkov <alex.matvey93@gmail.com>
2023-07-10 18:56:17 +0700

    made adaptive in the screen-1 section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './scss/app.scss';
+
+let header = document.querySelector('.header');
+let header__burger = document.querySelector('.header__burger');
+let header_menu = document.querySelector('.header__block-links');
+let lock = document.querySelector('body');
+
+header__burger.onclick = function () {
+  header.classList.toggle('active');
+  header__burger.classList.toggle('active');
+  header_menu.classList.toggle('active');
+  lock.classList.toggle('lock');
+};
